71ad8e9588f64727ebfa0f04d05b3576d1fa0bb6
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-03-20 23:15:49 +0300

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -6,9 +6,10 @@ import sqlite3
 import os
 
 # Create your views here.
-
+#index is the view.
 def index(request):
 
+    #insertName function is my part of app. this def returns a database that include api datas below.
     def insertName(name):
 
         with sqlite3.connect('celebsname.sqlite') as celebsname:
@@ -23,7 +24,7 @@ def index(request):
                 celebsname.commit()
             im.close()
             return celebsname
-
+    #this def returns 'name' information of parameter from "https://imdb8.p.rapidapi.com/actors/get-bio".
     def getName(x):
 
         url = "https://imdb8.p.rapidapi.com/actors/get-bio"
@@ -31,35 +32,34 @@ def index(request):
         querystring = {"nconst": x}
 
         headers = {
-            'x-rapidapi-key': "3ee414594bmsh95d08663d649adap12c8cbjsnf6e902276553",
+            'x-rapidapi-key': " ",#Before you try, you have to get a key from RapidApi.
             'x-rapidapi-host': "imdb8.p.rapidapi.com"
         }
 
         response = requests.request("GET", url, headers=headers, params=querystring)
-        result = json.loads(response.text)
+        result = json.loads(response.text)#listed "response.text" for index process
         return (result['name'])
 
-
+    #this part is for getting id datas from the api in below.( "https://imdb8.p.rapidapi.com/actors/list-most-popular-celebs")
     url = "https://imdb8.p.rapidapi.com/actors/list-most-popular-celebs"
 
     querystring = {"homeCountry":"US","currentCountry":"US","purchaseCountry":"US"}
 
     headers = {
-        'x-rapidapi-key': "3ee414594bmsh95d08663d649adap12c8cbjsnf6e902276553",
+        'x-rapidapi-key': " ",#Before you try, you have to get a key from RapidApi. 
         'x-rapidapi-host': "imdb8.p.rapidapi.com"
         }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     result = json.loads(response.text)
-    for i in range(0,10):
+    for i in range(0,10):#id's are sent "getName()"function one by one and added on database with "insertName"function.
         name = getName(result[i][6:-1])
         #print(name)
         insertName(name)
 
     im = insertName(None).cursor()
     im.execute("SELECT * FROM celebsname")
-    al = im.fetchall()
+    al = im.fetchall()#selected all of datas from "celebsname" db
 
 
-
-    return HttpResponse("{}".format(al))
+    return HttpResponse("{}".format(al))#published datas on homepage
