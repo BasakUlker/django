e712cca0cb4a979bd58edcac317780ea2e56134e
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-03-23 15:19:06 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import decorators,cache
+
+
+class Post(models.Model):
+
+    celebs_id = models.CharField(max_length=20, verbose_name =("ID"))
+    name = models.CharField(max_length=100, verbose_name=("NAME"))
+
+    def publish(self):
+        self.celebs_id = decorators.method_decorator(self.celebs_id, name='')
+        self.name = decorators.method_decorator(self.name, name='')
+        self.save()
+        return self.celebs_id,self.name
+    
+
+    def __str__(self):
+        return self.name
+
---
+++ views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .models import * 
+from django.shortcuts import HttpResponse
+from django.core.exceptions import MultipleObjectsReturned
+import requests
+import json
+import sqlite3
+import os
+def index(request):
+
+    def getName(x):
+
+        url = "https://imdb8.p.rapidapi.com/actors/get-bio"
+
+        querystring = {"nconst": x}
+
+        headers = {
+            'x-rapidapi-key': "709da9453fmsh5eac41924c01dd2p15fabcjsn2138cbc83099",
+            'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = json.loads(response.text)
+        return (result['name'])
+
+    url = "https://imdb8.p.rapidapi.com/actors/list-most-popular-celebs"
+
+    querystring = {"homeCountry":"US","currentCountry":"US","purchaseCountry":"US"}
+
+    headers = {
+        'x-rapidapi-key': "709da9453fmsh5eac41924c01dd2p15fabcjsn2138cbc83099",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = json.loads(response.text)
+    for i in range(0,5):
+        try: 
+            Post.objects.update_or_create(celebs_id=result[i][8:-1],name=getName(result[i][6:-1]))
+        except MultipleObjectsReturned as e:
+            print (e)
+    posts=Post.objects.distinct()
+    posts=Post.objects.values_list()
+    #posts=Post.objects.all().delete()
+    return HttpResponse(posts) 
+
