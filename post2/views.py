869c443aee2ffbbdc7638d2a986891e59e167698
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-04-16 23:53:19 +0300

    Update views.py

--- post2/views.py
+++ post2/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,HttpResponseRedirect, redirect
 from django.db import connections
 from .models import *
 from .tvShowsAndFilms import *
+from .forms import Post2Form
+from django.contrib import messages
+
 
 def post2_index(request):
 
-    return render(request,'post2/post2_index.html',{'posts':tvShowsNFilms()})
+    posts=Post2.objects.all()
+
+    if request.method == "POST":
+
+        posts = request.POST.getlist('posts[]')
+
+    context = {
+        'posts':posts,
+        }
+    return render(request,'post2/post2_index.html',context)
+
